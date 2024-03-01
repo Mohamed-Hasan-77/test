ffe8cc42c522a7b62e6cdd9627f55c818546e39c
Mohamed-Hasan-77 <0mdx3210@gmail.com>
2024-03-01 20:54:28 +0200

    f

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -56,7 +56,7 @@ export default function Hero() {
                         <div className="w-full mt-5">
                             <label htmlFor="phone" className="block mb-2 text-white text-sm font-medium text-gray-900 ">Your Phone Number (required)</label>
                             {/* <input type="tel" id="phone"  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:shadow-inputFocus focus:border-none focus:outline-none block w-full p-2  " pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required /> */}
-                            <PhoneInput country={'eg'} inputProps={{  required: true,  }} className="bg-gray-50  text-gray-900 text-sm rounded-lg focus:shadow-inputFocus focus:border-none focus:outline-none block w-full p-1  "/>
+                            <PhoneInput country={'eg'} inputProps={{  required: true,  }} className=" text-sm rounded-lg focus:shadow-inputFocus focus:border-none focus:outline-none block w-full p-1  "/>
                         </div>
 
                         <div className="w-full mt-5">
